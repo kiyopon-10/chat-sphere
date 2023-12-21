6ac1f51fd96d53eb39a9a591d4c9b94bf48fb896
Barshan <barshanbiswas2002@gmail.com>
2023-12-21 22:18:08 +0530

    Further work on database of server

--- chatSphere/server/models.py
+++ chatSphere/server/models.py
@@ -10,10 +10,25 @@ class Category(models.Model):
 
 class Server(models.Model):
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=250, null=True)
+    description = models.CharField(max_length=250, blank=True, null=True)
     ownername = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='server_owner')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='server_category')
     members = models.ManyToManyField(settings.AUTH_USER_MODEL)
     
+    def __str__(self):
+        return self.name
+    
 class Channel(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='channel_owner')
+    topic = models.CharField(max_length=100)
+    server = models.ForeignKey(Server, on_delete=models.CASCADE, related_name='channel_server')
+    
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)   #calling the original save method
+        
+    
+    def __str__(self):
+        return self.name
+    
